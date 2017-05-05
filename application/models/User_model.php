6b66ac23f4d6c57bb60835ff9cbbe52752b81b65
dantekitchen17 <dantekitchen17@gmail.com>
2017-05-05 18:05:07 +0700

    sedikit update2

--- application/models/User_model.php
+++ application/models/User_model.php
@@ -8,4 +8,24 @@ class User_model extends CI_Model{
     public function select_user(){
         return $this->db->query("select * from m_user order by user_id desc")->result_array();
     }
+	
+	public function updateExistingGroup($data) {
+		$this->db->where("user_id", $data["user_id"]);
+		$this->db->limit(1);
+		$updateData = array(
+			"group_name" => $data["group_name"],
+			"modified_by" => $data["modified_by"]
+		);
+		$this->db->update("m_group", $updateData);
+	}
+	
+	public function insertGroup($data) {
+		$insertData = array(
+			"group_name" => $data["group_name"],
+			"created_by" => $data["user_id"],
+			"modified_by" => $data["user_id"]
+		);
+		$this->db->insert("m_group", $insertData);
+		return $this->db->affected_rows();
+	}
 }
--- application/views/user.php
+++ application/views/user.php
@@ -2,7 +2,7 @@
 <div class="container-fluid">
 <div class="page-title"><?= $page_title ?></div>
 <div class="dialog-background">
-	<div class="dialog dialog-tambah">
+	<div class="dialog dialog-tambah-user">
 		<div class="dialog-header">
 			<div class="dialog-title">Tambah User</div>
 		</div>
@@ -10,31 +10,54 @@
 			<table>
 				<tbody>
 					<tr>
+						<td class="">Username</td>
+						<td><input type="text" class="input-username" /></td>
+					</tr>
+					<tr>
+						<td class="">Email</td>
+						<td><input type="text" class="input-user_email" maxlength="12" /></td>
+					</tr>
+					<tr>
 						<td class="">Nama</td>
-						<td><input type="text" class="input-nama" /></td>
+						<td><input type="text" class="input-user_fullname" /></td>
 					</tr>
 					<tr>
-						<td class="">Nopol Kendaraan</td>
-						<td><input type="text" class="input-nopol" maxlength="12" /></td>
+						<td class="">Alamat</td>
+						<td><input type="text" class="input-user_address" /></td>
 					</tr>
 					<tr>
-						<td class="">Keterangan</td>
-						<td><input type="text" class="input-keterangan" /></td>
+						<td class="">Telepon</td>
+						<td><input type="text" class="input-user_telephone" /></td>
 					</tr>
 					<tr>
-						<td class="">Status</td>
-						<td>
-							<select name="input-status" class="input-status">
-								<option value="1">Aktif</option>
-								<option value="0">Tidak Aktif</option>
-							</select>
-						</td>
+						<td class="">Handphone</td>
+						<td><input type="text" class="input-user_handphone" /></td>
 					</tr>
 				</tbody>
 			</table>
 		</div>
 		<div class="dialog-footer">
-			<button type="button" class="btn-default btn-submit-tambah">Tambah</button>
+			<button type="button" class="btn-default btn-submit-tambah-user">Tambah</button>
+		</div>
+	</div>
+</div>
+<div class="dialog-background">
+	<div class="dialog dialog-tambah-group">
+		<div class="dialog-header">
+			<div class="dialog-title">Tambah Group</div>
+		</div>
+		<div class="dialog-body">
+			<table>
+				<tbody>
+					<tr>
+						<td class="">Nama Grup</td>
+						<td><input type="text" class="input-group_name" /></td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+		<div class="dialog-footer">
+			<button type="button" class="btn-default btn-submit-tambah-group">Tambah</button>
 		</div>
 	</div>
 </div>
@@ -92,7 +115,7 @@
 </div>
 <div class="content">
 	<div class="section-1">
-		<button type="button" class="btn-default btn-tambah">Tambah User</button>
+		<button type="button" class="btn-default btn-tambah-user">Tambah User</button>
 		<table class="table table-user">
 			<thead>
 				<tr>
@@ -111,7 +134,7 @@
 		</table>
 	</div>
 	<div class="section-2">
-		<button type="button" class="btn-default btn-tambah">Tambah Group</button>
+		<button type="button" class="btn-default btn-tambah-group">Tambah Group</button>
 		<table class="table table-group">
 			<thead>
 				<tr>
@@ -131,7 +154,18 @@
 
 <script type="text/javascript">
 $(function() {
+	$(".btn-tambah-user").on("click", function() {
+		showDialog(".dialog-tambah-user");
+	});
 	
+	$(".btn-tambah-group").on("click", function() {
+		showDialog(".dialog-tambah-group");
+	});
 	
+	$(".dialog-background").on("click", function(e) {
+		if (e.target.className == "dialog-background") {
+			closeDialog();
+		}
+	});
 });
 </script>
