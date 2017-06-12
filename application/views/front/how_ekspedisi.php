0601b588e8643e8f4b9b1e3f8ec19d7c75f0c0bf
dantekitchen17 <dantekitchen17@gmail.com>
2017-06-12 14:28:06 +0700

    desain how it works

--- application/views/front/how_ekspedisi.php
+++ application/views/front/how_ekspedisi.php
@@ -4,9 +4,42 @@
 		<div class="section-1-text">
 			<div class="section-1-title">JANGAN BIARKAN TRUK ANDA KOSONG</div>
 			<div class="section-1-description">Yukirim menyediakan layanan dan fasilitas terbaik untuk menghemat waktumu dan tenagamu.</div>
+			<a href="<?= base_url("register#ekspedisi") ?>" class="btn btn-daftar">DAFTAR SEKARANG</a>
 		</div>
 	</div>
 	<div class="section-2">
-
+		<div class="section-title">How It Works</div>
+		<div class="section-2-item">
+			<div class="section-2-left" style="background-image: url('<?= base_url("assets/icons/ic_build_black_24dp_2x.png") ?>');"></div>
+			<div class="section-2-right">
+				<div class="section-2-item-title">LOREM IPSUM DOLOR</div>
+				<div class="horizontal-line"></div>
+				<div class="section-2-item-description">Lorem Ipsum adalah contoh teks atau dummy dalam industri percetakan dan penataan huruf atau typesetting. </div>
+			</div>
+		</div>
+		<div class="section-2-item">
+			<div class="section-2-left" style="background-image: url('<?= base_url("assets/icons/ic_description_black_24dp_2x.png") ?>');"></div>
+			<div class="section-2-right">
+				<div class="section-2-item-title">NASKAH LOREM IPSUM STANDAR</div>
+				<div class="horizontal-line"></div>
+				<div class="section-2-item-description">Sudah merupakan fakta bahwa seorang pembaca akan terpengaruh oleh isi tulisan dari sebuah halaman saat ia melihat tata letaknya.  </div>
+			</div>
+		</div>
+		<div class="section-2-item">
+			<div class="section-2-left" style="background-image: url('<?= base_url("assets/icons/ic_important_devices_black_24dp_2x.png") ?>');"></div>
+			<div class="section-2-right">
+				<div class="section-2-item-title">DE FINIBUS BONORUM ET MALORUM</div>
+				<div class="horizontal-line"></div>
+				<div class="section-2-item-description">Tidak seperti anggapan banyak orang, Lorem Ipsum bukanlah teks-teks yang diacak. Ia berakar dari sebuah naskah sastra latin klasik dari era 45 sebelum masehi, hingga bisa dipastikan usianya telah mencapai lebih dari 2000 tahun. </div>
+			</div>
+		</div>
+		<div class="section-2-item">
+			<div class="section-2-left" style="background-image: url('<?= base_url("assets/icons/ic_opacity_black_24dp_2x.png") ?>');"></div>
+			<div class="section-2-right">
+				<div class="section-2-item-title">ON THE OTHER HAND</div>
+				<div class="horizontal-line"></div>
+				<div class="section-2-item-description">Ada banyak variasi tulisan Lorem Ipsum yang tersedia, tapi kebanyakan sudah mengalami perubahan bentuk, entah karena unsur humor atau kalimat yang diacak hingga nampak sangat tidak masuk akal. </div>
+			</div>
+		</div>
 	</div>
 </div>
